7eefe06485b67c029abd81bdf26e6c37faf68ac9
Tuvshintur Batzorig <tuvshinturbatzorig@gmail.com>
2020-07-03 22:57:59 -0500

    function generics

--- features/generics.ts
+++ features/generics.ts
@@ -25,3 +25,35 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>(['a', 'b', 'c']);
+//also type inference works on generics
+const arrayofanthing = new ArrayOfAnything(['a', 'b', 'c']);
+
+// Example of generic with functions
+
+function printStrings(arr: string[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+function printNumber(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+// those code duplicates !!!
+// its time to use generics
+
+function printAnything<T>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+//also type inference works
+printAnything(['a', 'b', 'c']);
+
+//One last thing don't forget function return type
+//it will saves us from errors

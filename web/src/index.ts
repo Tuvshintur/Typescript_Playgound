38f2277e6354a9698674748a35286905561646da
Tuvshintur Batzorig <tuvshinturbatzorig@gmail.com>
2020-07-07 01:21:41 -0500

    collection view extended from view and completed web framework skeleton in TS

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,16 @@
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const collection = User.buildCollection();
-
-collection.on('change', () => {
-    console.log(collection);
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+    return User.buildUser(json);
 });
 
-collection.fetch();
+users.on('change', () => {
+    const root = document.getElementById('root');
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+
+users.fetch();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -20,4 +20,9 @@ export class User extends Model<UserProps> {
     static buildCollection(): Collection<User, UserProps> {
         return new Collection<User, UserProps>(rootUrl, (json: UserProps) => User.buildUser(json));
     }
+
+    setRandomAge(): void {
+        const age = Math.round(Math.random() * 100);
+        this.set({ age });
+    }
 }
